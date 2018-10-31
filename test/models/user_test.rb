1f805208685d7cdb4019b3f0ece9cd03dcc80e17
toriumi <saori.toriumi.54@gmail.com>
2018-11-01 03:12:05 +0900

    Fix user model is has secure password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,6 +4,7 @@ class UserTest < ActiveSupport::TestCase
 
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
+    password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
